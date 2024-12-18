57ed274285ba5f05a3807ff698abfcf4b025db62
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2024-12-18 14:42:10 +0530

    ABI path updated (#64)

--- packages/client/src/mud/setupNetwork.ts
+++ packages/client/src/mud/setupNetwork.ts
@@ -20,7 +20,7 @@ import { encodeEntity, syncToRecs } from "@latticexyz/store-sync/recs";
 
 import { getNetworkConfig } from "./getNetworkConfig";
 import { world } from "./world";
-import IWorldAbi from "contracts/out/IWorld.sol/IWorld.abi.json";
+import IWorldAbi from "./IWorld.abi.json";
 import { createBurnerAccount, getContract, transportObserver, ContractWrite } from "@latticexyz/common";
 
 import { Subject, share } from "rxjs";
